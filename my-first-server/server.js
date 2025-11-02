3a88a66f55e17f25d705978debabfbc25bea3665
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-11-02 22:23:35 +0600

    commit-1

--- my-first-server/server.js
+++ my-first-server/server.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) => {
 });
 
 const PORT = 5000;
+
+const user = [
+  {id:1, name:'Sabana',email:'sabana@gmail.com'},
+  {id:2, name:'Sabina',email:'sabina@gmail.com'},
+  {id:1, name:'Sammi',email:'sammi@gmail.com'},
+]
+app.get('/user',(req,res) =>{
+  res.send(user);
+})
+
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
