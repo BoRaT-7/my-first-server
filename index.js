481766f5896c983cf159490dab717cb437323f0c
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-11-03 22:08:14 +0600

    commit-1

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+  { id: 1, name: 'Sabina', email: 'sabina@gmail.com' },
+  { id: 2, name: 'Saba', email: 'saba@gmail.com' },
+];
+
+app.get('/user', (req, res) => {
+  res.send(users);
+});
+
 app.get('/', (req, res) => {
   res.send('User Management server is running');
 });
